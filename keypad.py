f8a0067f48f5b65f1d318128d362ce6add5c9a11
ainosato <56681766+ainosato@users.noreply.github.com>
2020-08-03 01:08:25 +0900

    Add files via upload

--- keypad.py
+++ keypad.py
@@ -3,6 +3,10 @@
 import sys
 
 import PyQt5
+import sys, pymysql
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QTreeView
+from PyQt5.QtGui import QStandardItemModel
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
@@ -14,9 +18,30 @@ MainUI = '../_uiFiles/main.ui'
 OutUI = '../_uiFiles/out.ui'
 OutcarUI = '../_uiFiles/outcar.ui'
 
-class KeypadDialog(QDialog):
+
+class MainDialog(QDialog):
     def __init__(self):
         QDialog.__init__(self, None)
+        uic.loadUi(MainUI, self)
+        self.in_pushButton.clicked.connect(self.go)
+        self.out_pushButton.clicked.connect(self.out)
+
+    def go(self):
+        self.close()
+        pad = KeypadDialog()
+        pad.exec_()
+
+    def out(self):
+        self.close()
+        out = outDialog()
+        out.exec_()
+
+
+class KeypadDialog(QDialog):
+    global ar;
+    def __init__(self):
+        QDialog.__init__(self, None)
+        self.sqlConnect()
         uic.loadUi(PadUI, self)
 
         self.num_pushButton_1.clicked.connect(lambda state, button = self.num_pushButton_1 : self.NumClicked(state, button))
@@ -31,8 +56,28 @@ class KeypadDialog(QDialog):
         self.num_pushButton_0.clicked.connect(lambda state, button = self.num_pushButton_0 : self.NumClicked(state, button))
 
         self.del_pushButton.clicked.connect(self.Delete)
-        self.enter_pushButton.clicked.connect(self.checkAnswer)
+        self.enter_pushButton.clicked.connect(self.addPassword)
         self.home_pushButton.clicked.connect(self.home)
+
+    def sqlConnect(self):
+        try:
+            self.conn = pymysql.connect(
+                host="localhost",
+                user="root",
+                password="apmsetup",
+                db="opentutorials",
+                port=3306,
+                charset="utf8"
+            )
+        except:
+            print("문제 발생")
+            exit(1)
+        print("연결 성공")
+        self.cur = self.conn.cursor()
+
+    def closeEvent(self, QCloseEvent):
+        print("close")
+        self.conn.close()
 
     def NumClicked(self, state, button):
         exist_line_text = self.q_lineEdit.text()
@@ -44,10 +89,24 @@ class KeypadDialog(QDialog):
         exist_line_text = exist_line_text[:-1]
         self.q_lineEdit.setText(exist_line_text)
 
+    def addPassword(self):
+        self.cmd = "INSERT INTO keypad('Password') VALUES (self.q_lineEdit.text())"
+        self.cur.execute(self.cmd)
+        self.conn.commit()
+        self.checkAnswer()
+
     def checkAnswer(self):
-        self.close()
-        Save = SaveDialog()
-        Save.exec_()
+        self.cmd = "SELECT * FROM keypad"
+        self.cur.execute(self.cmd)
+        self.conn.commit()
+        ar = self.cur.fetchall()
+        if(ar['Password'] == self.q_lineEdit.text()):
+            self.close()
+            Save = SaveDialog()
+            Save.exec_()
+        else:
+            print("실행 불가")
+
 
     def home(self):
         self.close()
@@ -66,22 +125,7 @@ class SaveDialog(QDialog):
         Main = MainDialog()
         Main.exec_()
 
-class MainDialog(QDialog):
-    def __init__(self):
-        QDialog.__init__(self, None)
-        uic.loadUi(MainUI, self)
-        self.in_pushButton.clicked.connect(self.go)
-        self.out_pushButton.clicked.connect(self.out)
 
-    def go(self):
-        self.close()
-        pad = KeypadDialog()
-        pad.exec_()
-
-    def out(self):
-        self.close()
-        out = outDialog()
-        out.exec_()
 
 class outDialog(QDialog):
     def __init__(self):
@@ -136,11 +180,8 @@ class OutcarDialog(QDialog):
         Main.exec_()
 
 
-
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_dialog = MainDialog()
-    main_dialog.show()
+    Main = MainDialog()
+    Main.show()
     app.exec_()
